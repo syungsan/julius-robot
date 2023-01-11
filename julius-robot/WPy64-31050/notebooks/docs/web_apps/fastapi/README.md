d2162dd2dd25dca58c9a5b1d55aa4f7c09dd7ebd
syungsan <syungsan@road.ocn.ne.jp>
2023-01-12 08:16:55 +0900

    さあ始めるぞ！

--- julius-robot/WPy64-31050/notebooks/docs/web_apps/fastapi/README.md
+++
@@ -1,7 +0,0 @@
-```console
-pip install fastapi
-pip install "uvicorn[standard]"
-pip install python-multipart sqlalchemy jinja2
-
-uvicorn app:app --reload
-```
--- julius-robot/start.bat
+++ julius-robot/start.bat
@@ -1,1 +1,1 @@
-dictation-kit-4.5\bin\windows\julius.exe -C dictation-kit-4.5\main.jconf -C dictation-kit-4.5\am-gmm.jconf -charconv utf-8 sjis -nolog -quiet -plugindir .
+dictation-kit-4.5\bin\windows\julius.exe -C dictation-kit-4.5\main.jconf -C dictation-kit-4.5\am-gmm.jconf -charconv utf-8 sjis -nolog -quiet -plugindir .\plugin
